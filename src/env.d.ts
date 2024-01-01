b69841c1cbda7f6a6607818f0cd8b172e90e693b
Faddah Wolf <my_biz@me.com>
2023-12-31 16:08:30 -0800

    added .env var types to to env.d.ts.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+	readonly MODE: string;
+	readonly PROD: boolean;
+	readonly DEV: boolean;
+	readonly SPOTIFY_CLIENT_ID: string;
+	readonly SPOTIFY_CLIENT_SECRET: string;
+	// more env variables...
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
